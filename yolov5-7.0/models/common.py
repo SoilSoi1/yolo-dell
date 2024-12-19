6e6dbfde80c186d86dec5e238fc6a1eb772b3125
SoilSoi1 <LuanYaoZJ@outlook.com>
2024-12-20 03:20:05 +0800

    Changes

--- yolov5-7.0/models/common.py
+++ yolov5-7.0/models/common.py
@@ -23,6 +23,7 @@ import torch
 import torch.nn as nn
 from IPython.display import display
 from PIL import Image
+from torch import Tensor
 from torch.cuda import amp
 
 from utils import TryExcept
@@ -858,3 +859,24 @@ class Classify(nn.Module):
         if isinstance(x, list):
             x = torch.cat(x, 1)
         return self.linear(self.drop(self.pool(self.conv(x)).flatten(1)))
+
+class SE(nn.Module):
+    def __init__(self, c1, c2, r=16):
+        super(SE, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.l1 = nn.Linear(c1, c1 // r, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.l2 = nn.Linear(c1 // r, c1, bias=False)
+        self.sig = nn.Sigmoid()
+    def forward(self, x):
+        print(x.size())
+        b, c, _, _ = x.size()
+        y = self.avgpool(x).view(b, c)
+        y = self.l1(y)
+        y = self.relu(y)
+        y = self.l2(y)
+        y = self.sig(y)
+        y = y.view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
